ee945f5fbcc6afebb0355c07d24ab95acdf95d28
guilhermefill <63548453+guilhermefill@users.noreply.github.com>
2022-10-31 17:40:14 +0100

    carousel route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -22,4 +22,27 @@ router.get('/details/:postId', async (req, res, next) => {
 	}
 });
 
+router.post('/carousel', async (req, res, next) => {
+	const { limit, offset } = req.body;
+	try {
+		const totalQuery = await Post.count();
+		if (totalQuery < offset) {
+			const carouselPost = await Post.find()
+				.populate('creator')
+				.skip(offset)
+				.limit(limit);
+			res.json(carouselPost);
+		} else {
+			let newSkip = offset % (totalQuery - 1);
+			const carouselPost = await Post.find()
+				.populate('creator')
+				.skip(newSkip)
+				.limit(limit);
+			res.json(carouselPost);
+		}
+	} catch (error) {
+		res.json(error);
+	}
+});
+
 module.exports = router;
